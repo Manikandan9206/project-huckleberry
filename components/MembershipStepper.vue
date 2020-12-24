dd9c899b194b6d987d02dcfdaae29ac0eb53676c
Manikandan9206 <manikandan9206@gmail.com>
2020-12-24 21:01:38 +0530

    stepper-2&3 done

--- components/MembershipStepper.vue
+++ components/MembershipStepper.vue
@@ -9,7 +9,9 @@
 
       <v-divider></v-divider>
 
-      <v-stepper-step :complete="e1 > 3" step="3"> Basic Ones </v-stepper-step>
+      <v-stepper-step :complete="e1 > 3" step="3" :rules="[() => (e1 > 3 ? basicDetailFormValid : true)]">
+        Basic Ones
+      </v-stepper-step>
 
       <v-divider></v-divider>
 
@@ -56,13 +58,71 @@
 
       <v-stepper-content step="2">
         <v-card>
-          <div class="card-div card-div-2"></div>
+          <div class="card-div card-div-2">
+            <div class="cont">
+              <h2>
+                DSC NIT Rourkela is filled with all kinds of people. Some of our teams are technical, creative, event
+                management and outreach. If you feel like you could fit in any of teams above, kindly move forward.
+              </h2>
+              <p>Learn More <a target="_blank" href="https://bit.ly/AboutDSCNITRourkela"> Here.</a></p>
+              <v-btn
+                tile
+                light
+                :large="$vuetify.breakpoint.mdAndUp"
+                :medium="$vuetify.breakpoint.smAndBelow"
+                class="my-btn explore-btn"
+                @click="e1 = 3"
+              >
+                Continue
+              </v-btn>
+            </div>
+          </div>
         </v-card>
       </v-stepper-content>
 
       <v-stepper-content step="3">
         <v-card>
-          <div class="card-div card-div-3"></div>
+          <div class="card-div card-div-3">
+            <div class="cont">
+              <v-form ref="basicDetailForm" v-model="basicDetailFormValid">
+                <div class="first-name">
+                  <h1>First up, What should we call you?</h1>
+                  <v-text-field
+                    v-model="member.firstName"
+                    :dense="$vuetify.breakpoint.xsOnly"
+                    label="First Name"
+                    outlined
+                    clearable
+                    :rules="[rules.required]"
+                  ></v-text-field>
+                </div>
+                <div class="last-name">
+                  <h1>Cool {{ member.firstName || 'buddy' }}, and your Last Name?</h1>
+                  <v-text-field
+                    v-model="member.lastName"
+                    :dense="$vuetify.breakpoint.xsOnly"
+                    label="Last Name"
+                    outlined
+                    clearable
+                  ></v-text-field>
+                </div>
+                <div class="email">
+                  <h1>Great, Now what's your email?</h1>
+                  <v-text-field
+                    v-model="member.email"
+                    :dense="$vuetify.breakpoint.xsOnly"
+                    label="E Mail"
+                    outlined
+                    clearable
+                    :rules="[rules.required, rules.email]"
+                  ></v-text-field>
+                </div>
+                <div class="submit">
+                  <v-btn tile light block class="my-btn" @click="submitBasicDetailForm"> Sign Up </v-btn>
+                </div>
+              </v-form>
+            </div>
+          </div>
         </v-card>
       </v-stepper-content>
 
@@ -111,7 +171,29 @@ export default {
     return {
       e1: 1,
       DSCCover: require('~/static/DSC_cover.png'),
+
+      member: {
+        firstName: '',
+        lastName: '',
+      },
+      basicDetailFormValid: true,
+      rules: {
+        required: (value) => !!value || 'Required.',
+        email: (value) => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || 'Invalid e-mail.'
+        },
+      },
     }
+  },
+  computed: {},
+  methods: {
+    submitBasicDetailForm() {
+      this.$refs.basicDetailForm.validate()
+      if (this.basicDetailFormValid) {
+        console.log(this.member)
+      }
+    },
   },
 }
 </script>
@@ -121,6 +203,7 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  padding: 2rem;
 
   @media (max-width: 800px) {
     flex-direction: column;
@@ -148,6 +231,44 @@ export default {
       font-size: 2rem;
     }
     &-action {
+    }
+  }
+}
+.card-div-2 {
+  padding: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .cont {
+    max-width: 600px;
+  }
+
+  h2 {
+    font-size: 3rem;
+    margin-bottom: 1rem;
+    @media (max-width: 500px) {
+      font-size: 2.5rem;
+    }
+  }
+  p {
+    font-size: 2rem;
+  }
+}
+
+.card-div-3 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .cont {
+    width: 75%;
+    max-width: 700px;
+  }
+  h1 {
+    font-size: 2.5rem;
+    margin-bottom: 1rem;
+    @media (max-width: 500px) {
+      font-size: 2rem;
     }
   }
 }
